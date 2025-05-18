8cf45435c5aa0935fd24b4ee264da9a7cd2fc0f0
Aarya <genzaarya@gmail.com>
2025-05-18 09:25:44 +0530

    Add footer section with email sign-up and social links; implement footer text animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { preloader } from "./modules/preloader.js";
+import { footerTextAnimation } from "./modules/footerTextAnimation.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     preloader();
+    footerTextAnimation();
 });
